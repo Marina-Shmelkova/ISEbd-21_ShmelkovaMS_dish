f8f242086276244b533c0834d9983ff879132d03
Марина Шмелькова <Shmelkova_Marina163@mail.ru>
2021-02-27 20:30:01 +0400

    Lab1

--- FoodOrders/FoodOrdersView/FormStorehouse.Designer.cs
+++ FoodOrders/FoodOrdersView/FormStorehouse.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FoodOrdersView
 {
-    partial class FormStoreHouse
+    partial class FormStorehouse
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.ColumnId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Component = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Count = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxResponsible = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelResponsible = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.InactiveCaption;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnId,
+            this.Component,
+            this.Count});
+            this.dataGridView.Location = new System.Drawing.Point(22, 97);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(343, 287);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // ColumnId
+            // 
+            this.ColumnId.HeaderText = "ColumnId";
+            this.ColumnId.Name = "ColumnId";
+            this.ColumnId.Visible = false;
+            // 
+            // Component
+            // 
+            this.Component.HeaderText = "Блюдо";
+            this.Component.Name = "Component";
+            this.Component.Width = 200;
+            // 
+            // Count
+            // 
+            this.Count.HeaderText = "Количество";
+            this.Count.Name = "Count";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(153, 12);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(155, 20);
+            this.textBoxName.TabIndex = 21;
+            // 
+            // textBoxResponsible
+            // 
+            this.textBoxResponsible.Location = new System.Drawing.Point(153, 54);
+            this.textBoxResponsible.Name = "textBoxResponsible";
+            this.textBoxResponsible.Size = new System.Drawing.Size(155, 20);
+            this.textBoxResponsible.TabIndex = 22;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(45, 15);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(96, 13);
+            this.labelName.TabIndex = 23;
+            this.labelName.Text = "Название склада";
+            // 
+            // labelResponsible
+            // 
+            this.labelResponsible.AutoSize = true;
+            this.labelResponsible.Location = new System.Drawing.Point(45, 57);
+            this.labelResponsible.Name = "labelResponsible";
+            this.labelResponsible.Size = new System.Drawing.Size(86, 13);
+            this.labelResponsible.TabIndex = 24;
+            this.labelResponsible.Text = "Ответственный";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(96, 408);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 26;
+            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.ButtonSave_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(203, 408);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 25;
+            this.buttonCancel.Text = "Отменить";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.ButtonCancel_Click);
+            // 
+            // FormStorehouse
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormStoreHouse";
+            this.ClientSize = new System.Drawing.Size(411, 460);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.labelResponsible);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.textBoxResponsible);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "FormStorehouse";
+            this.Text = "Добавление склада";
+            this.Load += new System.EventHandler(this.FormSetStorehouse_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxResponsible;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelResponsible;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Component;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Count;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonCancel;
     }
 }
--- FoodOrders/FoodOrdersView/FormStorehouse.cs
+++ FoodOrders/FoodOrdersView/FormStorehouse.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FoodOrdersBusinessLogic.BindingModels;
+using FoodOrdersBusinessLogic.BusinessLogics;
+using FoodOrdersBusinessLogic.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +10,102 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Unity;
 
 namespace FoodOrdersView
 {
-    public partial class FormStoreHouse : Form
+    public partial class FormStorehouse : Form
     {
-        public FormStoreHouse()
+        [Dependency]
+        public new IUnityContainer Container { get; set; }
+        public int Id { set { id = value; } }
+        private readonly StorehouseLogic logic;
+        private int? id;
+
+        private Dictionary<int, (string, int)> houseComponents;
+        public FormStorehouse(StorehouseLogic service)
         {
             InitializeComponent();
+            this.logic = service;
+        }
+        private void FormSetStorehouse_Load(object sender, EventArgs e)
+        {
+            if (id.HasValue)
+            {
+                try
+                {
+                    StorehouseViewModel view = logic.Read(new StorehouseBindingModel { Id = id.Value })?[0];
+                    if (view != null)
+                    {
+                        textBoxName.Text = view.StorehouseName;
+                        textBoxResponsible.Text = view.Responsible.ToString();
+                        houseComponents = view.StorehouseComponents;
+                        LoadData();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                houseComponents = new Dictionary<int, (string, int)>();
+            }
+        }
+        private void LoadData()
+        {
+            try
+            {
+                if (houseComponents != null)
+                {
+                    dataGridView.Rows.Clear();
+                    foreach (var pc in houseComponents)
+                    {
+                        dataGridView.Rows.Add(new object[] { pc.Key, pc.Value.Item1, pc.Value.Item2 });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void ButtonSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxName.Text))
+            {
+                MessageBox.Show("Заполните название", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(textBoxResponsible.Text))
+            {
+                MessageBox.Show("Заполните ответственного", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                logic.CreateOrUpdate(new StorehouseBindingModel
+                {
+                    Id = id,
+                    StorehouseName = textBoxName.Text,
+                    Responsible = textBoxResponsible.Text,
+                    DateCreate = DateTime.Now,
+                    StorehouseComponents = houseComponents
+                });
+                MessageBox.Show("Сохранение прошло успешно", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void ButtonCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
         }
     }
 }
